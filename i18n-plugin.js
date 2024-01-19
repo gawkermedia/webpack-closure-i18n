b3e1467da6ad83d8086e52b42065720e5a616769
Oleg Dyachenko <Dyachenko.oleg@gmail.com>
2024-01-19 16:56:37 +0700

    webpack 5 support - i18n-plugin.js - Update to webpack 5

--- i18n-plugin.js
+++ i18n-plugin.js
@@ -2,9 +2,8 @@
 
 'use strict';
 
-var path = require('path'),
-	ConcatSource = require('webpack/lib/ConcatSource'),
-	Translater = require('fast-closure-translater');
+const path = require('path');
+const Translater = require('fast-closure-translater');
 
 /**
  * Plugin for translating Closure Templates in already-built Webpack chunks.
@@ -12,9 +11,7 @@ var path = require('path'),
  * @param {object} config - configuration object
  * @param {array[string]} config.locales - Closure locale names to translate to
  * @param {string} config.localePath - Base path to load locale files from
- * @param {boolean} [config.appendSourceMapLink=false] - Whether to append a link to sourcemap in generated files
  * @param {string} [config.defaultLocale] - Default locale to use (e.g. when translation not available for specific locale)
- * @param {array[string]} [config.modules] - List of modules to translate (will translate all chunks if none specified here)
  * @param {string} [config.localeNameTemplateVar] - String to replace any instance of with locale name
  * @param {object} [config.localeAliases] - optional aliases for file path for a given locale. (e.g.:
  *	`{"en-US": "generated/extracted_msgs"}` if the `en-US` file path is `generated/extracted_msgs.xlf`
@@ -28,74 +25,70 @@ function PostCompileI18nPlugin(config) {
 		throw new Error('localePath must be set and pointed to the base path of locale files');
 	}
 
-	this.appendSourceMapLink = Boolean(config.appendSourceMapLink);
+	this.PLUGIN_NAME = 'PostCompileI18nPlugin';
 	this.defaultLocale = config.defaultLocale || 'en-US';
 	this.locales = config.locales;
 	this.localePath = config.localePath;
 	this.localeAliases = config.localeAliases || {};
-	this.modules = config.modules || [];
 	this.localeNameTemplateVar = config.localeNameTemplateVar || '[FAST_LOCALE]';
 }
 
-module.exports = PostCompileI18nPlugin;
+PostCompileI18nPlugin.prototype = {
+	constructor: PostCompileI18nPlugin,
 
-PostCompileI18nPlugin.prototype.apply = function (compiler) {
-	var instance = this;
+	apply: function (compiler) {
+		const self = this;
 
-	this.translater = null;
-	this.localeFiles = {};
+		const { ConcatSource } = compiler.webpack.sources;
 
-	// Load translation messages when compilation begins
-	function onRun(compiler, callback) {
-		var translater;
-		instance.locales.forEach(function (locale) {
-			instance.localeFiles[locale] = path.join(instance.localePath, (instance.localeAliases[locale] || locale) + '.xlf');
-		});
-		translater = instance.translater = new Translater({
-			localeFiles: instance.localeFiles,
-			defaultLocale: instance.defaultLocale,
-			replacementString: instance.localeNameTemplateVar
-		});
-		translater.messagesLoaded.then(function () {
-			callback();
-		}, function () {
-			throw new Error('Failed to load translations!');
-		});
-	}
-	compiler.plugin('run', onRun);
-	compiler.plugin('watch-run', onRun);
+		self.translater = null;
+		self.localeFiles = {};
 
-	compiler.plugin('compilation', function (compilation) {
+		// Load translation messages when compilation begins
+		const onRun = (compiler, callback) => {
+			self.locales.forEach(locale => {
+				const localeAlias = self.localeAliases[locale] || locale;
+				self.localeFiles[locale] = path.join(self.localePath, localeAlias + '.xlf');
+			});
+			const translater = new Translater({
+				localeFiles: self.localeFiles,
+				defaultLocale: self.defaultLocale,
+				replacementString: self.localeNameTemplateVar
+			});
 
-		compilation.plugin('additional-chunk-assets', function (chunks) {
-			function translateChunk(chunk) {
-				chunk.files.forEach(function (filename) {
-					var originalSource = compilation.assets[filename].source(),
-						translations = instance.translater.translate(originalSource);
-					instance.locales.forEach(function (locale) {
-						var localizedFilename = filename.replace(instance.localeNameTemplateVar, locale),
-							source = instance.appendSourceMapLink ? new ConcatSource(translations[locale] +
-							'\n//# sourceMappingURL=' + filename + '.map') : new ConcatSource(translations[locale]);
-						compilation.additionalChunkAssets.push(localizedFilename);
-						compilation.assets[localizedFilename] = source;
+			self.translater = translater;
+
+			translater.messagesLoaded
+				.then(
+					() => { callback(); },
+					() => { throw new Error('Failed to load translations!'); }
+				);
+		}
+
+		compiler.hooks.run.tapAsync(self.PLUGIN_NAME, onRun)
+		compiler.hooks.watchRun.tapAsync(self.PLUGIN_NAME, onRun)
+
+		compiler.hooks.compilation.tap(self.PLUGIN_NAME, compilation => {
+			compilation.hooks.processAssets.tap({
+				name: self.PLUGIN_NAME,
+				stage: 101 // @todo Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE + 1
+			}, assets => {
+				Object.entries(assets).forEach(([pathname, asset]) => {
+					const source = asset.source();
+					const translations = self.translater.translate(source);
+
+					self.locales.forEach(locale => {
+						const translatedPathname = pathname.replace(self.localeNameTemplateVar, locale);
+						const translatedSource = new ConcatSource(translations[locale]);
+
+						compilation.additionalChunkAssets.push(translatedPathname);
+						compilation.assets[translatedPathname] = translatedSource;
 					});
 				});
-			}
+			});
 
-			var filteredChunks;
-
-			if (instance.modules.length) {  // Module names specified - only translate these modules
-				filteredChunks = chunks.filter(function (chunk) {
-					if (!chunk.entry && !chunk.initial) {
-						// We need to translate all non-entry/non-initial chunks (e.g. lazy-loaded packages)
-						return true;
-					}
-					return (instance.modules.indexOf(chunk.name) > -1);
-				});
-			} else {  // Translate all modules
-				filteredChunks = chunks;
-			}
-			filteredChunks.forEach(translateChunk);
 		});
-	});
+	}
 };
+
+module.exports = PostCompileI18nPlugin;
